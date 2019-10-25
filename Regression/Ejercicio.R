246b24b8d3a269c3edcd9ce53e9d902a9749db09
Brian <bsmithsalamanca@gmail.com>
2019-10-24 21:59:20 -0500

    Add exercise 2

--- Regression/Ejercicio.R
+++ Regression/Ejercicio.R
@@ -1,17 +1,29 @@
-rm(list=ls())
-library(AmesHousing)
-library(rsample)
+## Ejercicio 2
 
-AmesHousing = make_ames()
-set.seed(123)
-ames_split <- initial_split(AmesHousing::make_ames(), prop = .7)
-ames_train <- training(ames_split)
-ames_test  <- testing(ames_split)
+library(dplyr)
+library(psych)
+library(GGally)
+library(ggplot2)
+library(gridExtra)
+library(lmtest)
+library(corrplot)
+library(car)
+library(ISLR)
+require(knitr)
+library(AER)
+library(stargazer)
+library(scales)
+library(MASS)
+library(mvtnorm)
+
+options(scipen=999)
 
 
-
-
-
-
-
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+dataset$State <- as.numeric(as.character(dataset$State))
 
